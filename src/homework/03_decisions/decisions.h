00644844dcb46ec6731dc162ce306e35a2988609
Pratik Khatiwada <pratik.khatiwada@g.austincc.edu>
2025-09-28 04:10:29 +0000

    hw03: add letter grade functions, main menu, and tests

--- src/homework/03_decisions/decisions.h
+++ src/homework/03_decisions/decisions.h
@@ -1,7 +1,12 @@
-//write include required statements
+#ifndef DECISIONS_H
+#define DECISIONS_H
 
+#include <string>
 
-//Write a function(s) prototype  here
+std::string get_letter_grade_using_if(int grade);
+std::string get_letter_grade_using_switch(int grade);
+
+#endif 
 
 
 
