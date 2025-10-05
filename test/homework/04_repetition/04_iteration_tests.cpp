75422fdb434e9bfc46ab5e224bc31c08a79bfc51
Pratik Khatiwada <pratik.khatiwada@g.austincc.edu>
2025-10-04 22:59:43 -0500

    Update 04_iteration_tests.cpp

--- test/homework/04_repetition/04_iteration_tests.cpp
+++ test/homework/04_repetition/04_iteration_tests.cpp
@@ -1,7 +1,18 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_MAIN
 #include "catch.hpp"
+#include "repetition.h"
 
-TEST_CASE("Verify Test Configuration", "verification") {
-	REQUIRE(true == true);
+TEST_CASE("Test get_factorial function")
+{
+    REQUIRE(get_factorial(3) == 6);
+    REQUIRE(get_factorial(5) == 120);
+    REQUIRE(get_factorial(6) == 720);
 }
+
+TEST_CASE("Test sum_odd_numbers function")
+{
+    REQUIRE(sum_odd_numbers(5) == 9);   // 1 + 3 + 5 = 9
+    REQUIRE(sum_odd_numbers(10) == 25); // 1 + 3 + 5 + 7 + 9 = 25
+}
+
 
