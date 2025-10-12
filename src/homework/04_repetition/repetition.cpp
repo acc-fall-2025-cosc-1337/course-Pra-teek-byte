c6a86311ce2bb60746cd1f23bd48100317ba5785
Pratik Khatiwada <pratik.khatiwada@g.austincc.edu>
2025-10-12 16:14:49 -0500

    Update repetition.cpp

--- src/homework/04_repetition/repetition.cpp
+++ src/homework/04_repetition/repetition.cpp
@@ -30,39 +30,3 @@ int sum_odd_numbers(int num)
 
     return sum;
 }
-<<<<<<< HEAD
-=======
-
-=======
-
-int get_factorial(int num)
-{
-    int result = 1;
-
-    // Using for loop to calculate factorial
-    for (int i = 1; i <= num; ++i)
-    {
-        result *= i;
-    }
-
-    return result;
-}
-
-int sum_odd_numbers(int num)
-{
-    int sum = 0;
-    int i = 1;
-
-    // Using while loop to add odd numbers up to 'num'
-    while (i <= num)
-    {
-        if (i % 2 != 0)
-        {
-            sum += i;
-        }
-        i++;
-    }
-
-    return sum;
-}
-
