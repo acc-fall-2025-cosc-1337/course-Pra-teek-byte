165b96fc4f92434eb3ec5a17b26a82b11796afd8
Pratik Khatiwada <pratik.khatiwada@g.austincc.edu>
2025-10-12 04:54:00 +0000

    Complete Homework 05: DNA functions and tests

--- src/homework/04_repetition/repetition.cpp
+++ src/homework/04_repetition/repetition.cpp
@@ -1,4 +1,5 @@
 #include "repetition.h"
+<<<<<<< HEAD
 
 int get_factorial(int num)
 {
@@ -31,3 +32,36 @@ int sum_odd_numbers(int num)
     return sum;
 }
 
+=======
+
+int get_factorial(int num)
+{
+    int result = 1;
+
+    // Using for loop to calculate factorial
+    for (int i = 1; i <= num; ++i)
+    {
+        result *= i;
+    }
+
+    return result;
+}
+
+int sum_odd_numbers(int num)
+{
+    int sum = 0;
+    int i = 1;
+
+    // Using while loop to add odd numbers up to 'num'
+    while (i <= num)
+    {
+        if (i % 2 != 0)
+        {
+            sum += i;
+        }
+        i++;
+    }
+
+    return sum;
+}
+>>>>>>> aa1a573 (Homework 4: Completed menu program and unit tests for factorial & sum of odd numbers)
--- src/homework/04_repetition/repetition.h
+++ src/homework/04_repetition/repetition.h
@@ -1,6 +1,12 @@
+<<<<<<< HEAD
 
 #include <iostream>
 using namespace std;
 
+=======
+#include <iostream>
+using namespace std;
+
+>>>>>>> aa1a573 (Homework 4: Completed menu program and unit tests for factorial & sum of odd numbers)
 int get_factorial(int num);
 int sum_odd_numbers(int num);
