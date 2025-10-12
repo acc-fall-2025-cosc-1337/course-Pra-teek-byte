288a28769477c93f8d0748b90d381cb3bcb3d00d
Pratik Khatiwada <pratik.khatiwada@g.austincc.edu>
2025-10-12 16:15:27 -0500

    Update repetition.h

--- src/homework/04_repetition/repetition.h
+++ src/homework/04_repetition/repetition.h
@@ -1,27 +1,9 @@
-<<<<<<< HEAD
 #include <iostream>
 using namespace std;
 
 int get_factorial(int num);
 int sum_odd_numbers(int num);
 
-#include <iostream>
-using namespace std;
 
-int get_factorial(int num);
-int sum_odd_numbers(int num);
 
-=======
-<<<<<<< HEAD
 
-#include <iostream>
-using namespace std;
-
-=======
-#include <iostream>
-using namespace std;
-
->>>>>>> aa1a573 (Homework 4: Completed menu program and unit tests for factorial & sum of odd numbers)
-int get_factorial(int num);
-int sum_odd_numbers(int num);
->>>>>>> hw05_fixed
