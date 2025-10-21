1f1031262d9fb647083139c37313e60459b088cd
Pratik Khatiwada <pratik.khatiwada@g.austincc.edu>
2025-10-21 23:26:05 +0000

    05_functions tests

--- src/homework/05_functions/main.cpp
+++ src/homework/05_functions/main.cpp
@@ -1,6 +1,42 @@
+#include <iostream>
+#include "func.h"
+using namespace std;
 
-int main() 
+int main()
 {
+    int choice;
+    string dna;
 
-	return 0;
+    do
+    {
+        cout << "\nMenu:\n";
+        cout << "1 - Get GC Content\n";
+        cout << "2 - Get DNA Complement\n";
+        cout << "3 - Exit\n";
+        cout << "Enter choice: ";
+        if (!(cin >> choice)) { cin.clear(); cin.ignore(10000,'\n'); continue; }
+
+        if (choice == 1)
+        {
+            cout << "Enter DNA string: ";
+            cin >> dna;
+            cout << "GC Content: " << get_gc_content(dna) << endl;
+        }
+        else if (choice == 2)
+        {
+            cout << "Enter DNA string: ";
+            cin >> dna;
+            cout << "DNA Complement: " << get_dna_complement(dna) << endl;
+        }
+        else if (choice == 3)
+        {
+            cout << "Exiting program.\n";
+        }
+        else
+        {
+            cout << "Invalid choice. Try again.\n";
+        }
+    } while (choice != 3);
+
+    return 0;
 }
