7c3e762cee353853ec6b5ea0e7beabee26242fae
Pratik Khatiwada <pratik.khatiwada@g.austincc.edu>
2025-11-01 23:07:01 -0500

    TicTacToe

--- src/homework/06_tic_tac_toe/tic_tac_toe.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe.h
@@ -1,1 +1,29 @@
+#ifndef TIC_TAC_TOE_H
+#define TIC_TAC_TOE_H
+
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+
+class TicTacToe
+{
+public:
+    bool game_over();
+    void start_game(string first_player);
+    void mark_board(int position);
+    string get_player() const;
+    void display_board() const;
+
+private:
+    void set_next_player();
+    bool check_board_full();
+    void clear_board();
+
+    string player;
+    vector<string> pegs{9, " "}; 
+};
+
+#endif
 //h
